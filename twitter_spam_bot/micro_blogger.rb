a20cea53e27fdc44c3a8f190490cce7c48abe13d
arialblack14 <tsirasalexis@yahoo.gr>
2015-10-05 23:29:58 +0300

    Added a better user interface

--- twitter_spam_bot/micro_blogger.rb
+++ twitter_spam_bot/micro_blogger.rb
@@ -8,14 +8,31 @@ class MicroBlogger
 	end
 
 	def tweet message
-		puts message.length
+		puts "Message length is : " + message.length.to_s
 		if message.length.between?(0, 140)
 			@client.update message
 		else
 			puts "Message should be 140 characters max."
 		end
 	end
+
+	def run
+		command = ""
+		puts "This program may spam your twitter account, so be careful when using it."
+		while command != "q"
+			printf "Enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+				when "q" then puts "Goodbye!"
+				when "t" then tweet(parts[1..-1].join(" ")) unless parts[1..-1].nil?
+				else
+					puts "Sorry, i don't know how to #{command}."				
+			end
+		end
+	end
 end
 
 blogger = MicroBlogger.new
-blogger.tweet "aaaaMr MicroBloggerMicroBlogger MicroBlogger MicroBlogger MicroBlogger  MicroBlogger MicroBlogger M MicroBlogger MicroBlogger MicroBlogger!!"
+blogger.run
