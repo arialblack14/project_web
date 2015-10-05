261db1cedc64b9886b6f231ae21ac1aefad309f6
arialblack14 <tsirasalexis@yahoo.gr>
2015-10-06 00:16:32 +0300

    Annoying all followers

--- twitter_spam_bot/micro_blogger.rb
+++ twitter_spam_bot/micro_blogger.rb
@@ -27,9 +27,37 @@ class MicroBlogger
 			case command
 				when "q" then puts "Goodbye!"
 				when "t" then tweet(parts[1..-1].join(" ")) unless parts[1..-1].nil?
+				when "dm" then dm(parts[1], parts[2..-1].join(" "))
+				when "spam" then spam_my_followers parts[1..-1]
 				else
 					puts "Sorry, i don't know how to #{command}."				
 			end
+		end
+	end
+
+	def dm target, message
+		puts "Trying to send #{target} this direct message:"
+		puts message
+		screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+		if screen_names.include? target
+			message = "dm @#{target} #{message}"
+			tweet message
+		else
+			puts "You are trying to message someone who is a non-follower."
+		end
+	end
+
+	def followers_list
+		screen_names = []
+		@client.followers.each do |follower|
+			screen_names << @client.user(follower).screen_name
+		end
+		screen_names
+	end
+
+	def spam_my_followers message
+		followers_list.each do |screen_name|
+			dm(screen_name, message.to_s.join(" "))
 		end
 	end
 end
